611b4f07f93707b7ee8e166c365ec5c88e597362
JonataBecker <jonatabecker@gmail.com>
2016-04-23 09:52:39 -0300

    Criado View responsável pelas informações de tempo de revisão x número de incidentes

--- buscaDados.sql
+++ buscaDados.sql
@@ -2,18 +2,20 @@
 /* Tempo de produção */
 CREATE OR REPLACE VIEW TEMPO_PRODUCAO_SUB AS
 	SELECT 
-			ROUND(totpro / 60) AS TEMPO, 
-			numero_incidentes_producao 
-		FROM dados
-		WHERE 
-			totpro > 60;
+		ROUND(totpro / 60) AS TEMPO, 
+		numero_incidentes_producao 
+	FROM 
+		dados
+	WHERE 
+		totpro > 60;
 
 CREATE OR REPLACE VIEW TEMPO_PRODUCAO AS
 	SELECT 
 		tempo, 
 		AVG(numero_incidentes_producao) * 30 AS mediaNumeroIncidentes, 
 		COUNT(numero_incidentes_producao) numeroIncidentes 
-	FROM TEMPO_PRODUCAO_SUB
+	FROM 
+		TEMPO_PRODUCAO_SUB
 	GROUP BY 
 		tempo
 	HAVING 
@@ -27,17 +29,32 @@ SELECT * FROM TEMPO_PRODUCAO;
 /* Tempo de revisão */
 
 
-SELECT REVISAO, AVG(numero_incidentes_producao) * 30, COUNT(*) T FROM (
-	SELECT ROUND((rev_programacao * 100) / totpro) AS REVISAO, totrevpro, totpro, numero_incidentes_producao FROM dados 
-	WHERE totpro > 60 AND totpro > rev_programacao
-    HAVING REVISAO > 0 AND REVISAO < 90
-	ORDER BY REVISAO DESC
-) T
-GROUP BY REVISAO
-HAVING T > 10
+CREATE OR REPLACE VIEW TEMPO_REVISAO_SUB AS
+	SELECT 
+		ROUND((rev_programacao * 100) / totpro) AS tempo, 
+        totrevpro, 
+        totpro, 
+        numero_incidentes_producao 
+	FROM 
+		dados 
+	WHERE 
+		totpro > 60 AND 
+        totpro > rev_programacao
+    HAVING
+		tempo > 0 AND
+		tempo < 90
+	ORDER BY 
+		tempo DESC;
+
+CREATE OR REPLACE VIEW TEMPO_REVISAO AS 
+	SELECT 
+		tempo, 
+        AVG(numero_incidentes_producao) * 30 as mediaNumeroIncidentes,
+        COUNT(*) numeroIncidentes
+	FROM 
+		TEMPO_REVISAO_SUB
+	GROUP BY 
+		tempo
+	HAVING 
+		numeroIncidentes > 10
 ;
-
-
-SELECT ROUND((totrevpro * 100) / totpro) AS REVISAO, totrevpro, totpro, numero_incidentes FROM dados
-WHERE totpro > 60 AND totpro > totrevpro
-ORDER BY REVISAO DESC;
